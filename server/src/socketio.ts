2c6bbdee225721471be7a9fe880202add7ca8095
Pedro Miotti Arribamar <pedromiotti7@gmail.com>
2021-05-04 09:49:54 -0300

    Update socketio.ts

--- server/src/socketio.ts
+++ server/src/socketio.ts
@@ -17,33 +17,20 @@ export = function createConnection(http: any) {
   const io = ioServer(http, options);
 
   io.on(SocketEvents.CLIENT_CONNECT, (socket: ioServer.Socket) => {
+    
     console.log("connected " + socket.id);
 
-    socket.on(SocketEvents.CLIENT_UPDATE_STATUS, ({ userId }) =>
-      UserService.UserConnected(userId, socket.id)
-    );
+    socket.on(SocketEvents.CLIENT_UPDATE_STATUS, ({ userId }) => UserService.UserConnected(userId, socket.id));
 
-    socket.on(
-      SocketEvents.CLIENT_INVITE_PLAYER,
-      ({ matchId, opponentId, ownerId }) =>
-        MatchService.SendInvite(matchId, opponentId, ownerId, io)
-    );
+    socket.on(SocketEvents.CLIENT_INVITE_PLAYER, ({ matchId, opponentId, ownerId }) => MatchService.SendInvite(matchId, opponentId, ownerId, io));
 
-    socket.on(SocketEvents.CLIENT_JOIN_MATCH, ({ matchId, userId }) =>
-      MatchService.JoinRoom(matchId, userId, socket, io)
-    );
+    socket.on(SocketEvents.CLIENT_JOIN_MATCH, ({ matchId, userId }) => MatchService.JoinRoom(matchId, userId, socket, io));
 
-    socket.on(SocketEvents.CLIENT_USER_READY, ({ matchId, userId }) =>
-      MatchService.SetUserReady(matchId, userId, io, socket)
-    );
+    socket.on(SocketEvents.CLIENT_USER_READY, ({ matchId, userId }) => MatchService.SetUserReady(matchId, userId, io, socket));
 
-    socket.on(SocketEvents.CLIENT_ANSWER_QUESTION, ({ matchId, userId, questionId, answerId, correctAnswer }) =>
-      MatchService.AnswerQuestion(userId, matchId, questionId, answerId, correctAnswer)
-    );
+    socket.on(SocketEvents.CLIENT_ANSWER_QUESTION, ({ matchId, userId, questionId, answerId, correctAnswer }) => MatchService.AnswerQuestion(userId, matchId, questionId, answerId, correctAnswer));
 
-    socket.on(SocketEvents.CLIENT_DISCONNECT, () =>
-      MatchService.DisconnectUserFromMatch(socket.id, io, socket)
-    );
+    socket.on(SocketEvents.CLIENT_DISCONNECT, () => MatchService.DisconnectUserFromMatch(socket.id, io, socket));
 
   });
 
