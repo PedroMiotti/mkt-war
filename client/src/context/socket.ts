e5f2fa8efc4a3fa39aedb5d17f79f963063b74a3
Pedro <pedromiotti7@gmail.com>
2021-06-06 12:08:51 -0300

    Socket url config

--- client/src/context/socket.ts
+++ client/src/context/socket.ts
@@ -2,5 +2,5 @@ import React from 'react';
 import socketio from "socket.io-client";
 
 //http://localhost:3001
-export const socket: SocketIOClient.Socket = socketio.connect("http://111.11.111.111/");
+export const socket: SocketIOClient.Socket = socketio.connect("http://111.11.111.111/api-v1");
 export const SocketContext = React.createContext(socket);
